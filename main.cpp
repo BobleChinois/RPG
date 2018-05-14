2791604c85a59734750f83444ca3d5ccf022d19f
Boblechinois <alexis.gaub@yahoo.fr>
2018-05-14 12:44:30 +0200

    initialisation

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "Personnage.h"
+
+
+using namespace std;
+
+int main()
+{
+    Personnage ian, canigou("Crocs", 20, 20);
+
+    ian.attaquer(canigou);
+    canigou.attaquer(ian);
+    cout<<"Tour 1"<<endl;
+    cout<<"Ian"<<endl;
+    ian.afficherEtat();
+    cout<<"Canigou"<<endl;
+    canigou.afficherEtat();
+    ian.soin(20);
+    canigou.attaquer(ian);
+    cout<<"Tour 2"<<endl;
+    cout<<"Ian"<<endl;
+    ian.afficherEtat();
+    cout<<"Canigou"<<endl;
+    canigou.afficherEtat();
+    ian.changerArme("Double Shotgun",100);
+    ian.attaquer(canigou);
+    cout<<"Tour 3"<<endl;
+    cout<<"Ian"<<endl;
+    ian.afficherEtat();
+    cout<<"Canigou"<<endl;
+    canigou.afficherEtat();
+    return 0;
+}
